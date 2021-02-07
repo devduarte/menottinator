97c1c90cf3d3478daca3ffd2ce2e437c5624e6f9
Diego <dduarte@manas.com.ar>
2021-02-07 03:49:36 -0300

    Menottinator first working version

---
+++ index.js
@@ -0,0 +1,48 @@
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+    options: { debug: true },
+    connection: {
+        secure: true,
+        reconnect: true
+    },
+    identity: {
+        username: 'menottinator',
+        password: 'oauth:8h9msixp1pzqo2v0tk4qjln8f1v69z'
+    },
+    channels: ['dieduarte']
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+    // Ignore echoed messages.
+    if(self) return;
+
+    function setResultados() {
+        return ["El ewok desfigurado.",
+            "El rey de los contenidos.",
+            "El mismisimo Flaco Menouri.",
+            "El pastas.", "Lauricate (miralo vos,es global con 2 horas jugadas).",
+            "El bigote del Sr Tuzo.",
+            "Les mejores mods de la tierra.",
+            "La coquita de cada dia.",
+            "DJcorchacitos.",
+            "La peabody de Mastecs.",
+            "Los rolitos de la Clandenotti.",
+            "El joven prodigio Tomks.",
+            "El tilteo en el Fall Guys.",
+            "l bot carajeado.",
+            "Un ghostero.",
+            "Un cheetero que se va a ir al infierno.",
+            "La timba de cada stream.",
+            "El Keru tilteado."
+        ]
+    }
+
+    const resultados = setResultados()
+    var item = resultados[Math.floor(Math.random() * resultados.length)];
+    if(message.toLowerCase() === '!cn') {
+        client.say(channel, `@${tags.username}, En el mundo de Clandenotti serías: ` + item);
+    }
+});
