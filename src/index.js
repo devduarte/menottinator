b17e167fbddbdc8585262702adf9ad26c3291112
Diego <dduarte@manas.com.ar>
2021-02-08 02:22:33 -0300

    Added response when someone tags the bot

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const express = require('express');
 const app = express();
+const botname = 'menottinator'
 
 app.listen(process.env.PORT, () =>
     console.log('Menottinator ' + process.env.PORT || 80)
@@ -16,7 +17,7 @@ const client = new tmi.Client({
         reconnect: true
     },
     identity: {
-        username: 'menottinator',
+        username: 'botname',
         password: process.env.TWITCH_OAUTH_TOKEN
     },
     channels: ['flacomenotti']
@@ -47,7 +48,8 @@ client.on('message', (channel, tags, message, self) => {
             "Un cheetero que se va a ir al infierno.",
             "La timba de cada stream.",
             "El Keru tilteado.",
-            "El Antisexo."
+            "El Antisexo.",
+            "ya es tarde, bro. mañana te digo"
         ]
     }
 
@@ -55,5 +57,12 @@ client.on('message', (channel, tags, message, self) => {
     var item = resultados[Math.floor(Math.random() * resultados.length)];
     if(message.toLowerCase() === '!cn') {
         client.say(channel, `@${tags.username}, En el mundo de Clandenotti serías: ` + item);
+    }
+    if(message.toLowerCase().contains(`@${botname}`)){
+        const rest2 = ["los bots no stremeamos.", "fui programado para que me guste el vino patero.", "no te hagas el cheto, flaco",
+        "01100011 01101111 01101101 01100001 01101110 01101101 01100101 00100000 01101100 01101111 00100000 01100100 01101111 00100000 01101000 01110101 01100101 01110110 01101111",
+        "no ganan una partida ni en pedo", "<3 <3 yo tambien", "vos tambien <3 <3", "que onda amigue?"]
+        var item2 = rest2[Math.floor(Math.random() * resultados.length)];
+        client.say(channel, `@${tags.username} ` + item2);
     }
 });
